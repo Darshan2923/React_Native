38ed9110720c6778090b5dc964ac08d6a084818c
Darshan <darshanpatel2923@gmail.com>
2024-12-26 22:50:17 +0530

    Added Screen options statically

--- React_Native_Navigation_Meals_App/App.js
+++ React_Native_Navigation_Meals_App/App.js
@@ -15,8 +15,12 @@ export default function App() {
     <>
       <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name='MealsCategories' component={CategoriesPage} />
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: '#351410' },
+          headerTintColor: 'white',
+          contentStyle: { backgroundColor: '#3f2f25' },
+        }}>
+          <Stack.Screen name='MealsCategories' component={CategoriesPage} options={{ title: 'All Categories' }} />
           <Stack.Screen name='MealsOverview' component={MealsOverviewPage} />
         </Stack.Navigator>
         {/* <CategoriesPage/> */}
