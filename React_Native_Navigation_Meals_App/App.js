06db975c1be3b5653dd54f48acce18101630453e
Darshan <darshanpatel2923@gmail.com>
2024-12-27 21:01:41 +0530

    Done with react navigation and nesting different navigation styles

--- React_Native_Navigation_Meals_App/App.js
+++ React_Native_Navigation_Meals_App/App.js
@@ -6,9 +6,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import CategoriesPage from './pages/CategoriesPage';
 import MealsOverviewPage from './pages/MealsOverviewPage';
 import MealsDetails from './pages/MealsDetails';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Favourites from './pages/Favourites';
+import { Ionicons } from '@expo/vector-icons';
 
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return <Drawer.Navigator screenOptions={{
+    headerStyle: { backgroundColor: '#351410' },
+    headerTintColor: 'white',
+    sceneStyle: { backgroundColor: '#3f2f25' },
+    drawerContentStyle: { backgroundColor: '#351401' },
+    drawerInactiveTintColor: 'white',
+    drawerActiveTintColor: '#351401',
+    drawerActiveBackgroundColor: '#e4baa1'
+  }}>
+    <Drawer.Screen name='Categories' component={CategoriesPage} options={{ title: 'All Categories', drawerIcon: ({ color, size }) => <Ionicons color={color} size={size} name='list' /> }} />
+    <Drawer.Screen name='Favourites' component={Favourites} options={{ title: 'Favourites', drawerIcon: ({ color, size }) => <Ionicons color={color} size={size} name='star' /> }} />
+  </Drawer.Navigator>
+}
 
 
 export default function App() {
@@ -21,6 +40,7 @@ export default function App() {
           headerTintColor: 'white',
           contentStyle: { backgroundColor: '#3f2f25' },
         }}>
+          <Stack.Screen name='Drawer' component={DrawerNavigator} options={{ headerShown: false }} />
           <Stack.Screen name='MealsCategories' component={CategoriesPage} options={{ title: 'All Categories' }} />
           <Stack.Screen name='MealsOverview' component={MealsOverviewPage}
           // options={({route,navigation})=>{
@@ -30,7 +50,7 @@ export default function App() {
           //   };
           // }}
           />
-          <Stack.Screen name='MealsDetails' component={MealsDetails}
+          <Stack.Screen name='MealsDetails' component={MealsDetails} options={{ title: 'About the meal' }}
           // options={{
           //   headerRight:()=>{
           //     return <Button title='Tap me' onPress={()=>{console.log('Pressed')}}/>
---
+++ React_Native_Navigation_Meals_App/pages/Favourites.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Favourites = () => {
+    return (
+        <View>
+            <Text>This is Favourites screen</Text>
+        </View>
+    )
+}
+
+export default Favourites
