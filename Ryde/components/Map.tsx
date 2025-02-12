12c9e54299df73df47c55966240e2cf9cf9ec6b6
Darshan <darshanpatel2923@gmail.com>
2025-02-12 21:21:24 +0530

    Done with map functionality for our home page

--- Ryde/components/Map.tsx
+++ Ryde/components/Map.tsx
@@ -1,14 +1,22 @@
 import { View } from 'react-native';
 import React from 'react';
 import MapView, { PROVIDER_DEFAULT, Marker } from 'react-native-maps';
+import { useLocationStore } from '@/store';
+import { calculateRegion } from '@/lib/map';
 
 const Map = () => {
-    const region = {
-        latitude: 19.217958,     // Latitude of Padma Nagar, Kandivali East
-        longitude: 72.867332,   // Longitude of Padma Nagar, Kandivali East
-        latitudeDelta: 0.01,    // Zoom level
-        longitudeDelta: 0.01,   // Zoom level
-    };
+    // const region = {
+    //     latitude: 19.217958,     // Latitude of Padma Nagar, Kandivali East
+    //     longitude: 72.867332,   // Longitude of Padma Nagar, Kandivali East
+    //     latitudeDelta: 0.01,    // Zoom level
+    //     longitudeDelta: 0.01,   // Zoom level
+    // };
+
+    const { userLongitude, userLatitude, destinationLatitude, destinationLongitude } = useLocationStore();
+
+    const region = calculateRegion({
+        userLongitude, userLatitude, destinationLatitude, destinationLongitude
+    })
 
 
     return (
