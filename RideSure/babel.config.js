de042953c11811e6bb1da6536860b4f150787f38
Darshan <darshanpatel2923@gmail.com>
2025-03-08 22:56:00 +0530

    Done with the error for our google autocomplete and google api functionality.

--- RideSure/babel.config.js
+++ RideSure/babel.config.js
@@ -2,8 +2,7 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: [
-            '@babel/preset-env',
-            '@babel/preset-react'
+            'babel-preset-expo'
         ],
         plugins: [
             [
--- RideSure/screens/HomeScreen.js
+++ RideSure/screens/HomeScreen.js
@@ -1,12 +1,30 @@
 import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setDestination, setOrigin } from '../slices/navSlice'
 
 const HomeScreen = () => {
+
+    // useEffect(() => {
+    //     const checkApi = async () => {
+    //         try {
+    //             const response = await fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Delhi&key=${GOOGLE_MAPS_APIKEY}`);
+    //             const data = await response.json();
+    //             console.log('API is working:', data);
+    //         } catch (error) {
+    //             console.error('Error checking API:', error);
+    //         }
+    //     };
+
+    //     checkApi();
+    // }, []);
+
+    const dispatch = useDispatch();
 
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
@@ -29,13 +47,29 @@ const HomeScreen = () => {
                     minLength={2}
                     fetchDetails={true}
                     onPress={(data, details = null) => {
-                        console.log(data, details);
+                        // console.log(data, details);
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description: data.description
+                        }));
+
+                        dispatch(setDestination(null));
                     }}
                     query={{
                         key: GOOGLE_MAPS_APIKEY,
                         language: 'en',
+                        components: 'country:in',
                     }}
-                    nearbyPlacesAPI="GooglePlacesSearch"
+                    nearbyPlacesAPI="places"
+                    enablePoweredByContainer={false}
+                    returnKeyType={'search'}
+                    listViewDisplayed="auto"
+                    textInputProps={{
+                        clearButtonMode: 'while-editing',
+                        autoCorrect: false
+                    }}
+
+
                     debounce={400}
                 />
                 <NavOptions />
