001f58eb5396f2f2006173d2900ab769f2066c5c
Darshan <darshanpatel2923@gmail.com>
2025-03-08 21:41:48 +0530

    Done with google auto complete

---
+++ RideSure/babel.config.js
@@ -0,0 +1,18 @@
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+        ],
+        plugins: [
+            [
+                "module:react-native-dotenv",
+                {
+                    moduleName: "@env",
+                    path: ".env",
+                },
+            ],
+        ],
+    };
+};
--- RideSure/screens/HomeScreen.js
+++ RideSure/screens/HomeScreen.js
@@ -3,15 +3,41 @@ import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
 const HomeScreen = () => {
+
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`pl-5 pr-5`}>
                 <Image
                     style={{ width: 150, height: 100, resizeMode: 'contain' }}
-                    source={require('../assets/logo.png')} />
+                    source={require('../assets/logo.png')}
+                />
 
+                <GooglePlacesAutocomplete
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        },
+                    }}
+                    placeholder="Search"
+                    minLength={2}
+                    fetchDetails={true}
+                    onPress={(data, details = null) => {
+                        console.log(data, details);
+                    }}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                />
                 <NavOptions />
             </View>
         </SafeAreaView>
